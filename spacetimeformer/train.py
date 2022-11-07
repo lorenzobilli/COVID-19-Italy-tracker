f7e17d5936b7e661eef90c912bc52194d30b66da
Lorenzo Billi <lorenzoaebilli@gmail.com>
2022-11-07 18:25:29 +0100

    Removed unused models and started cleaninig up train.py

--- spacetimeformer/train.py
+++ spacetimeformer/train.py
@@ -10,8 +10,6 @@ import torch
 
 import spacetimeformer as stf
 
-_MODELS = ["spacetimeformer", "mtgnn", "heuristic", "lstm", "lstnet", "linear", "s4"]
-
 _DSETS = [
     "asos",
     "metr-la",
@@ -35,17 +33,12 @@ _DSETS = [
 
 
 def create_parser():
-    model = sys.argv[1]
-    dset = sys.argv[2]
+    dset = sys.argv[1]
 
     # Throw error now before we get confusing parser issues
-    assert (
-        model in _MODELS
-    ), f"Unrecognized model (`{model}`). Options include: {_MODELS}"
     assert dset in _DSETS, f"Unrecognized dset (`{dset}`). Options include: {_DSETS}"
 
     parser = ArgumentParser()
-    parser.add_argument("model")
     parser.add_argument("dset")
 
     if dset == "precip":
@@ -77,25 +70,10 @@ def create_parser():
         stf.data.CSVTorchDset.add_cli(parser)
     stf.data.DataModule.add_cli(parser)
 
-    if model == "lstm":
-        stf.lstm_model.LSTM_Forecaster.add_cli(parser)
-        stf.callbacks.TeacherForcingAnnealCallback.add_cli(parser)
-    elif model == "lstnet":
-        stf.lstnet_model.LSTNet_Forecaster.add_cli(parser)
-    elif model == "mtgnn":
-        stf.mtgnn_model.MTGNN_Forecaster.add_cli(parser)
-    elif model == "heuristic":
-        stf.heuristic_model.Heuristic_Forecaster.add_cli(parser)
-    elif model == "spacetimeformer":
-        stf.spacetimeformer_model.Spacetimeformer_Forecaster.add_cli(parser)
-    elif model == "linear":
-        stf.linear_model.Linear_Forecaster.add_cli(parser)
-    elif model == "s4":
-        stf.s4_model.S4_Forecaster.add_cli(parser)
+    stf.spacetimeformer_model.Spacetimeformer_Forecaster.add_cli(parser)
 
     stf.callbacks.TimeMaskedLossCallback.add_cli(parser)
 
-    parser.add_argument("--wandb", action="store_true")
     parser.add_argument("--plot", action="store_true")
     parser.add_argument("--plot_samples", type=int, default=8)
     parser.add_argument("--attn_plot", action="store_true")
@@ -191,186 +169,71 @@ def create_model(config):
     assert yc_dim is not None
     assert yt_dim is not None
 
-    if config.model == "lstm":
-        forecaster = stf.lstm_model.LSTM_Forecaster(
-            # encoder
-            d_x=x_dim,
-            d_yc=yc_dim,
-            d_yt=yt_dim,
-            time_emb_dim=config.time_emb_dim,
-            hidden_dim=config.hidden_dim,
-            n_layers=config.n_layers,
-            dropout_p=config.dropout_p,
-            # training
-            learning_rate=config.learning_rate,
-            teacher_forcing_prob=config.teacher_forcing_start,
-            l2_coeff=config.l2_coeff,
-            loss=config.loss,
-            linear_window=config.linear_window,
-            use_revin=config.use_revin,
-            linear_shared_weights=config.linear_shared_weights,
-            use_seasonal_decomp=config.use_seasonal_decomp,
-        )
-
-    elif config.model == "heuristic":
-        forecaster = stf.heuristic_model.Heuristic_Forecaster(
-            d_x=x_dim,
-            d_yc=yc_dim,
-            d_yt=yt_dim,
-            context_points=config.context_points,
-            target_points=config.target_points,
-            loss=config.loss,
-            method=config.method,
-        )
-    elif config.model == "mtgnn":
-        forecaster = stf.mtgnn_model.MTGNN_Forecaster(
-            d_x=x_dim,
-            d_yc=yc_dim,
-            d_yt=yt_dim,
-            context_points=config.context_points,
-            target_points=config.target_points,
-            gcn_depth=config.gcn_depth,
-            dropout_p=config.dropout_p,
-            node_dim=config.node_dim,
-            dilation_exponential=config.dilation_exponential,
-            conv_channels=config.conv_channels,
-            subgraph_size=config.subgraph_size,
-            skip_channels=config.skip_channels,
-            end_channels=config.end_channels,
-            residual_channels=config.residual_channels,
-            layers=config.layers,
-            propalpha=config.propalpha,
-            tanhalpha=config.tanhalpha,
-            learning_rate=config.learning_rate,
-            kernel_size=config.kernel_size,
-            l2_coeff=config.l2_coeff,
-            time_emb_dim=config.time_emb_dim,
-            loss=config.loss,
-            linear_window=config.linear_window,
-            linear_shared_weights=config.linear_shared_weights,
-            use_seasonal_decomp=config.use_seasonal_decomp,
-            use_revin=config.use_revin,
-        )
-    elif config.model == "lstnet":
-        forecaster = stf.lstnet_model.LSTNet_Forecaster(
-            d_x=x_dim,
-            d_yc=yc_dim,
-            d_yt=yt_dim,
-            context_points=config.context_points,
-            hidRNN=config.hidRNN,
-            hidCNN=config.hidCNN,
-            hidSkip=config.hidSkip,
-            CNN_kernel=config.CNN_kernel,
-            skip=config.skip,
-            dropout_p=config.dropout_p,
-            output_fun=config.output_fun,
-            learning_rate=config.learning_rate,
-            l2_coeff=config.l2_coeff,
-            loss=config.loss,
-            linear_window=config.linear_window,
-            use_revin=config.use_revin,
-        )
-    elif config.model == "spacetimeformer":
-        if hasattr(config, "context_points") and hasattr(config, "target_points"):
-            max_seq_len = config.context_points + config.target_points
-        elif hasattr(config, "max_len"):
-            max_seq_len = config.max_len
-        else:
-            raise ValueError("Undefined max_seq_len")
-        forecaster = stf.spacetimeformer_model.Spacetimeformer_Forecaster(
-            d_x=x_dim,
-            d_yc=yc_dim,
-            d_yt=yt_dim,
-            max_seq_len=max_seq_len,
-            start_token_len=config.start_token_len,
-            attn_factor=config.attn_factor,
-            d_model=config.d_model,
-            d_queries_keys=config.d_qk,
-            d_values=config.d_v,
-            n_heads=config.n_heads,
-            e_layers=config.enc_layers,
-            d_layers=config.dec_layers,
-            d_ff=config.d_ff,
-            dropout_emb=config.dropout_emb,
-            dropout_attn_out=config.dropout_attn_out,
-            dropout_attn_matrix=config.dropout_attn_matrix,
-            dropout_qkv=config.dropout_qkv,
-            dropout_ff=config.dropout_ff,
-            pos_emb_type=config.pos_emb_type,
-            use_final_norm=not config.no_final_norm,
-            global_self_attn=config.global_self_attn,
-            local_self_attn=config.local_self_attn,
-            global_cross_attn=config.global_cross_attn,
-            local_cross_attn=config.local_cross_attn,
-            performer_kernel=config.performer_kernel,
-            performer_redraw_interval=config.performer_redraw_interval,
-            attn_time_windows=config.attn_time_windows,
-            use_shifted_time_windows=config.use_shifted_time_windows,
-            norm=config.norm,
-            activation=config.activation,
-            init_lr=config.init_lr,
-            base_lr=config.base_lr,
-            warmup_steps=config.warmup_steps,
-            decay_factor=config.decay_factor,
-            initial_downsample_convs=config.initial_downsample_convs,
-            intermediate_downsample_convs=config.intermediate_downsample_convs,
-            embed_method=config.embed_method,
-            l2_coeff=config.l2_coeff,
-            loss=config.loss,
-            class_loss_imp=config.class_loss_imp,
-            recon_loss_imp=config.recon_loss_imp,
-            time_emb_dim=config.time_emb_dim,
-            null_value=config.null_value,
-            pad_value=config.pad_value,
-            linear_window=config.linear_window,
-            use_revin=config.use_revin,
-            linear_shared_weights=config.linear_shared_weights,
-            use_seasonal_decomp=config.use_seasonal_decomp,
-            use_val=not config.no_val,
-            use_time=not config.no_time,
-            use_space=not config.no_space,
-            use_given=not config.no_given,
-            recon_mask_skip_all=config.recon_mask_skip_all,
-            recon_mask_max_seq_len=config.recon_mask_max_seq_len,
-            recon_mask_drop_seq=config.recon_mask_drop_seq,
-            recon_mask_drop_standard=config.recon_mask_drop_standard,
-            recon_mask_drop_full=config.recon_mask_drop_full,
-        )
-    elif config.model == "linear":
-        forecaster = stf.linear_model.Linear_Forecaster(
-            d_x=x_dim,
-            d_yc=yc_dim,
-            d_yt=yt_dim,
-            context_points=config.context_points,
-            learning_rate=config.learning_rate,
-            l2_coeff=config.l2_coeff,
-            loss=config.loss,
-            linear_window=config.linear_window,
-            linear_shared_weights=config.linear_shared_weights,
-            use_revin=config.use_revin,
-            use_seasonal_decomp=config.use_seasonal_decomp,
-        )
-    elif config.model == "s4":
-        forecaster = stf.s4_model.S4_Forecaster(
-            context_points=config.context_points,
-            target_points=config.target_points,
-            d_state=config.d_state,
-            d_model=config.d_model,
-            d_x=x_dim,
-            d_yc=yc_dim,
-            d_yt=yt_dim,
-            layers=config.layers,
-            time_emb_dim=config.time_emb_dim,
-            channels=config.channels,
-            dropout_p=config.dropout_p,
-            learning_rate=config.learning_rate,
-            l2_coeff=config.l2_coeff,
-            loss=config.loss,
-            linear_window=config.linear_window,
-            linear_shared_weights=config.linear_shared_weights,
-            use_revin=config.use_revin,
-            use_seasonal_decomp=config.use_seasonal_decomp,
-        )
+    if hasattr(config, "context_points") and hasattr(config, "target_points"):
+        max_seq_len = config.context_points + config.target_points
+    elif hasattr(config, "max_len"):
+        max_seq_len = config.max_len
+    else:
+        raise ValueError("Undefined max_seq_len")
+    forecaster = stf.spacetimeformer_model.Spacetimeformer_Forecaster(
+        d_x=x_dim,
+        d_yc=yc_dim,
+        d_yt=yt_dim,
+        max_seq_len=max_seq_len,
+        start_token_len=config.start_token_len,
+        attn_factor=config.attn_factor,
+        d_model=config.d_model,
+        d_queries_keys=config.d_qk,
+        d_values=config.d_v,
+        n_heads=config.n_heads,
+        e_layers=config.enc_layers,
+        d_layers=config.dec_layers,
+        d_ff=config.d_ff,
+        dropout_emb=config.dropout_emb,
+        dropout_attn_out=config.dropout_attn_out,
+        dropout_attn_matrix=config.dropout_attn_matrix,
+        dropout_qkv=config.dropout_qkv,
+        dropout_ff=config.dropout_ff,
+        pos_emb_type=config.pos_emb_type,
+        use_final_norm=not config.no_final_norm,
+        global_self_attn=config.global_self_attn,
+        local_self_attn=config.local_self_attn,
+        global_cross_attn=config.global_cross_attn,
+        local_cross_attn=config.local_cross_attn,
+        performer_kernel=config.performer_kernel,
+        performer_redraw_interval=config.performer_redraw_interval,
+        attn_time_windows=config.attn_time_windows,
+        use_shifted_time_windows=config.use_shifted_time_windows,
+        norm=config.norm,
+        activation=config.activation,
+        init_lr=config.init_lr,
+        base_lr=config.base_lr,
+        warmup_steps=config.warmup_steps,
+        decay_factor=config.decay_factor,
+        initial_downsample_convs=config.initial_downsample_convs,
+        intermediate_downsample_convs=config.intermediate_downsample_convs,
+        embed_method=config.embed_method,
+        l2_coeff=config.l2_coeff,
+        loss=config.loss,
+        class_loss_imp=config.class_loss_imp,
+        recon_loss_imp=config.recon_loss_imp,
+        time_emb_dim=config.time_emb_dim,
+        null_value=config.null_value,
+        pad_value=config.pad_value,
+        linear_window=config.linear_window,
+        use_revin=config.use_revin,
+        linear_shared_weights=config.linear_shared_weights,
+        use_seasonal_decomp=config.use_seasonal_decomp,
+        use_val=not config.no_val,
+        use_time=not config.no_time,
+        use_space=not config.no_space,
+        use_given=not config.no_given,
+        recon_mask_skip_all=config.recon_mask_skip_all,
+        recon_mask_max_seq_len=config.recon_mask_max_seq_len,
+        recon_mask_drop_seq=config.recon_mask_drop_seq,
+        recon_mask_drop_standard=config.recon_mask_drop_standard,
+        recon_mask_drop_full=config.recon_mask_drop_full,
+    )
 
     return forecaster
 
@@ -687,9 +550,6 @@ def create_callbacks(config, save_dir):
             )
         )
 
-    if config.wandb:
-        callbacks.append(pl.callbacks.LearningRateMonitor())
-
     if config.model == "lstm":
         callbacks.append(
             stf.callbacks.TeacherForcingAnnealCallback(
@@ -719,30 +579,6 @@ def main(args):
     if not os.path.exists(log_dir):
         os.makedirs(log_dir)
 
-    if args.wandb:
-        import wandb
-
-        project = os.getenv("STF_WANDB_PROJ")
-        entity = os.getenv("STF_WANDB_ACCT")
-        assert (
-            project is not None and entity is not None
-        ), "Please set environment variables `STF_WANDB_ACCT` and `STF_WANDB_PROJ` with \n\
-            your wandb user/organization name and project title, respectively."
-        experiment = wandb.init(
-            project=project,
-            entity=entity,
-            config=args,
-            dir=log_dir,
-            reinit=True,
-        )
-        config = wandb.config
-        wandb.run.name = args.run_name
-        wandb.run.save()
-        logger = pl.loggers.WandbLogger(
-            experiment=experiment,
-            save_dir=log_dir,
-        )
-
     # Dset
     (
         data_module,
@@ -766,48 +602,6 @@ def main(args):
     callbacks = create_callbacks(args, save_dir=log_dir)
     test_samples = next(iter(data_module.test_dataloader()))
 
-    if args.wandb and args.plot:
-        callbacks.append(
-            stf.plot.PredictionPlotterCallback(
-                test_samples,
-                var_idxs=plot_var_idxs,
-                var_names=plot_var_names,
-                pad_val=pad_val,
-                total_samples=min(args.plot_samples, args.batch_size),
-            )
-        )
-
-    if args.wandb and args.dset in ["mnist", "cifar"] and args.plot:
-        callbacks.append(
-            stf.plot.ImageCompletionCallback(
-                test_samples,
-                total_samples=min(16, args.batch_size),
-                mode="left-right" if config.dset == "mnist" else "flat",
-            )
-        )
-
-    if args.wandb and args.dset == "copy" and args.plot:
-        callbacks.append(
-            stf.plot.CopyTaskCallback(
-                test_samples,
-                total_samples=min(16, args.batch_size),
-            )
-        )
-
-    if args.wandb and args.model == "spacetimeformer" and args.attn_plot:
-
-        callbacks.append(
-            stf.plot.AttentionMatrixCallback(
-                test_samples,
-                layer=0,
-                total_samples=min(16, args.batch_size),
-            )
-        )
-
-    if args.wandb:
-        config.update(args)
-        logger.log_hyperparams(config)
-
     if args.val_check_interval <= 1.0:
         val_control = {"val_check_interval": args.val_check_interval}
     else:
@@ -816,7 +610,7 @@ def main(args):
     trainer = pl.Trainer(
         gpus=args.gpus,
         callbacks=callbacks,
-        logger=logger if args.wandb else None,
+        logger=None,
         accelerator="dp",
         gradient_clip_val=args.grad_clip_norm,
         gradient_clip_algorithm="norm",
@@ -837,9 +631,6 @@ def main(args):
     # forecaster.to("cuda")
     # xc, yc, xt, _ = test_samples
     # yt_pred = forecaster.predict(xc, yc, xt)
-
-    if args.wandb:
-        experiment.finish()
 
 
 if __name__ == "__main__":
